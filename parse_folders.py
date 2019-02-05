633ce97442d7848e95c9a561901828cef10b9e14
silverben10 <bensilverman10@gmail.com>
2019-02-05 09:37:24 +0000

    New file to add folder name functionality

---
+++ parse_folders.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+def parse_folders(filepath):
+    folderpath = os.path.join(filepath, os.pardir, "boostnote.json")
+
+    with open(folderpath, "r") as f:
+        parsed_json = json.load(f)
+        
+    folders = parsed_json["folders"]
+
+    f_dict = {}
+
+    for i in folders:
+        f_dict[i["key"]] = i["name"]
+
+    return f_dict   
